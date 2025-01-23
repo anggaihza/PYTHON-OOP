813084392a9803e5d3036ae41882e94d822ed353
Asep Angga Ihza S <asep@senja.co.uk>
2025-01-23 09:19:35 +0700

    inheritance

--- main.py
+++ main.py
@@ -1,9 +1,36 @@
 from car import Car
 
-car1 = Car('Mustang', 2024, 'red', False)
 
-print(car1.model)
-print(car1.color)
-car1.drive()
-car1.stop()
-car1.describe()
+class Animal:
+    def __init__(self, name):
+        self.name = name
+        self.is_alive = False
+
+    def eat(self):
+        print(f"{self.name} is eating")
+
+    def sleep(self):
+        print(f"{self.name} is sleeping")
+
+class Dog(Animal):
+    pass
+
+class Cat(Animal):
+    pass
+
+dog = Dog("Scooby")
+cat = Cat("Garfield")
+
+
+# print(car1.model)
+# print(car1.color)
+# car1.drive()
+# car1.stop()
+# car1.describe()
+
+print(dog.name)
+print(dog.is_alive)
+print(cat.name)
+cat.eat()
+
+# print(f"My graduating class of {Student.class_year} has {Student.num_students} students")
